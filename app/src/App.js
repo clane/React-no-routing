d6c12baa0986c821dfad3570858eca0bc0a84d59
Chris Lane <chris.lane359@gmail.com>
2018-12-07 08:07:40 -0800

    basic example with no routing

---
+++ app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Component1 from "./Component1.js";
+import Component2 from "./Component2.js";
+import Component3 from "./Component3.js";
+import Component4 from "./Component4.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Components with No Routing</h1>
+			  <Component1/>
+			  <Component2/>
+			  <Component3/>
+			  <Component4/>
+      </div>
+    );
+  }
+}
+
+export default App;
